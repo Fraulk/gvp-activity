1567c5a527b8a9951b1aa5ef5d13c02d2659976a
f <fratulker@gmail.com>
2024-04-14 21:30:18 +0200

    add: style

--- packages/client/src/components/VoiceChannelActivity.tsx
+++ packages/client/src/components/VoiceChannelActivity.tsx
@@ -1,10 +1,17 @@
-import * as React from 'react';
-import {Player} from './Player';
-import {usePlayers} from '../hooks/usePlayers';
-import './VoiceChannelActivity.css';
+import { Player } from "./Player";
+import { usePlayers } from "../hooks/usePlayers";
+import "./VoiceChannelActivity.css";
+import { useEffect } from "react";
 
 export function VoiceChannelActivity() {
   const players = usePlayers();
+
+//   simulates lots of players
+//   useEffect(() => {
+//     players.push(...players);
+//     players.push(...players);
+//     players.push(...players);
+//   }, []);
 
   return (
     <div className="voice__channel__container">
